5b25b89edc86725336c36e7762cff9dbf933a54b
mrsk8lalo <5395mrsk8lalo@users.noreply.github.com>
2018-05-06 09:14:23 -0500

    Mod DBA
    Modificación de la base de datos

--- GYM_IS/GYM/GYM/settings.py
+++ GYM_IS/GYM/GYM/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'GYM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'usuarios',
-        'USER': 'root',
-        'PASSWORD': 'premium5395',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'usuarios',
+        #'USER': 'root',
+        #'PASSWORD': 'premium5395',
+        #'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        #'PORT': '3306',
     }
 }
 
