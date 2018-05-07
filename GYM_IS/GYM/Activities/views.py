bb0fec7a332f1e12f43e244dac982270e217a8e3
mrsk8lalo <5395mrsk8lalo@users.noreply.github.com>
2018-05-07 14:11:36 -0500

    No inserta en la base de datos
    No funciona un poco mi codigo al insertar en la base de datos

--- GYM_IS/GYM/Activities/views.py
+++ GYM_IS/GYM/Activities/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
-
+from django.template import loader
+from .models import Ubicacion,Tiposact,Empleados
 from django.db import connections
 from django.db.utils import OperationalError
-
+from django.shortcuts import render
 
 
 def index(request):
-
-    return HttpResponse("Hello, world. You're at the polls index.");
+    all_Ubica = Ubicacion.objects.all()
+    all_Tipos = Tiposact.objects.all()
+    all_Empleados = Empleados.objects.all()
+   # all_Actividades = Activity.objects.all()
+    context = {
+        'all_Ubica': all_Ubica,
+        'all_Tipos': all_Tipos,
+        'all_Empleados': all_Empleados
+       # 'all_Actividades':all_Actividades
+               }
+    return render(request, 'Actividades.html',context);
