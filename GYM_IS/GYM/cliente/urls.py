e2c7d5f3e97cb8a3cff9f3115b96da27966eccc7
Valerdi0 <dani_9_valerdi@hotmail.com>
2018-06-02 23:38:02 -0500

    Pantalla de cursos y membresias ya terminadas si acaso solo agregar algo mas en los cursos, ya cuentan con imagen

--- GYM_IS/GYM/cliente/urls.py
+++ GYM_IS/GYM/cliente/urls.py
@@ -2,19 +2,24 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(
-		regex=r'^$',
-		view=views.index,
-		name='index'
-	),
-	url(
-		regex=r'^membresias/$',
-		view=views.Membresias,
-		name='membresias'
-	),
-	url(
-		regex=r'^login/$',
-		view=views.Login,
-		name='login'
-	),																													
+    url(
+        regex=r'^$',
+        view=views.index,
+        name='index'
+    ),
+    url(
+        regex=r'^membresias/$',
+        view=views.Membresias,
+        name='membresias'
+    ),
+    url(
+        regex=r'^login/$',
+        view=views.Login,
+        name='login'
+    ),
+    url(
+        regex=r'^cursos/$',
+        view=views.Cursos,
+        name='cursos'
+    ),
 ]
--- GYM_IS/GYM/cliente/views.py
+++ GYM_IS/GYM/cliente/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-	
+
+
 def index(request):
-	"""View que muestra la pagina principal"""
-	
-	return render(request, 'cliente/index.html',{})
-	#render(request,template_name,context) en este caso como es el index el contexto esta vacio porque no necesitamos nada.
+    """View que muestra la pagina principal"""
+    return render(request, 'cliente/index.html', {})
+    """render(request,template_name,context) en este caso como es el index el 
+    contexto esta vacio porque no necesitamos nada."""
+
+
 def Membresias(request):
-	"""View que muestra la pagina de membresias"""
-	return render(request,'cliente/Membresia.html',{})
+    """View que muestra la pagina de membresias"""
+    return render(request, 'cliente/Membresia.html', {})
+
+
 def Login(request):
-	"""View que muestra la pagina de membresias"""
-	return render(request,'cliente/login.html',{})
+    """View que muestra la pagina de membresias"""
+    return render(request, 'cliente/login.html', {})
+
+
+def Cursos(request):
+    """View que muestra la pagina de cursos"""
+    return render(request, 'cliente/cursos.html', {})
